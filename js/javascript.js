daeccc88d113e83840b3f686f99cda40b72f8511
Gabriel Porto Oliveira <gabrielpoliveira@ig.com.br>
2018-05-18 18:16:32 -0300

    0.3.1
    Implementando as porcentagens

--- js/javascript.js
+++ js/javascript.js
@@ -3,6 +3,12 @@ document.getElementById('botao').addEventListener("click",enviar,false);
 function enviar() {
 
     var tipo = document.getElementById('tipo').value;
+    var tabelas = document.getElementById('tabelas');
+    if(document.getElementById('total').textContent.trim() == "0"){
+
+        tabelas.className = "row";
+    }
+
     if(tipo == "+"){
 
         ganho();
@@ -14,21 +20,32 @@ function enviar() {
 }
 
 function despesa(){
+    //tota de despesas
     var despesa = document.getElementById('despesa');
+    //novo valor
     var atual = document.getElementById('valor');
+    //novo total de despesas
     var novo = parseFloat(despesa.textContent) + parseFloat(atual.value)
 
-    //inserindo na tabela
-    document.getElementById("table-body-despesa").innerHTML += "<tr><td>"+desc.value+"</td><td>"+atual.value+"</td></tr>"
-
+    //modificando o total de despesas
     despesa.innerHTML='';
     despesa.innerHTML=novo;
+
+    //calculando a porcentagem
+    var porcento =  parseFloat(document.getElementById('despesa').textContent.trim()) / parseFloat(document.getElementById('ganho').textContent.trim())  *100
+    //inserindo na tabela
+    document.getElementById("table-body-despesa").innerHTML += "<tr><td>"+desc.value+"</td><td>"+atual.value+"</td><td>"+porcento+"</td></tr>"
+
+
     total()
 }
 
 function ganho() {
+    //tota de ganhos
     var ganho = document.getElementById('ganho');
+    //novo valor
     var atual = document.getElementById('valor');
+    //novo total de ganhos
     var desc = document.getElementById('desc')
     //inserindo na tabela
     document.getElementById("table-body-ganho").innerHTML += "<tr><td>"+desc.value+"</td><td>"+atual.value+"</td></tr>"
@@ -50,7 +67,7 @@ function total(){
     total.innerHTML='';
     total.innerHTML=novo;
 
-    if(novo > 0 ){
+    if(novo >= 0 ){
 
         total.className = "alert "+ " alert-success"
     }else{
